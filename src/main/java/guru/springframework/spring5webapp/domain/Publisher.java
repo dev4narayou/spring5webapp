d88bda1e37962d4c92ab4539b509fae31604842e
dev <devmanojkumar08@gmail.com>
2025-01-21 15:49:04 +1100

    Section 2.22. Added Thymeleaf rendering of Books and Publisher.

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,113 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+// this is a JPA entity
+// this means that it is a POJO that is stored in a database
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String addressLine1;
+    private String zip;
+    private String city;
+    private String state;
+
+    @OneToMany
+    @JoinColumn(name = "publisher_id") // creates a foreign key in the book table
+    private Set<Book> books = new HashSet<>();
+
+    public Publisher() {
+    }
+
+    public Publisher(String name, String addressLine1, String zip, String city, String state) {
+        this.name = name;
+        this.addressLine1 = addressLine1;
+        this.zip = zip;
+        this.city = city;
+        this.state = state;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", addressLine1='" + addressLine1 + '\'' +
+                ", zip='" + zip + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Publisher)) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+
+}
--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -3,5 +3,10 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
+// This is a Spring Data JPA Repository
+// It is an interface that extends CrudRepository
+// It takes the domain object type to manage and the id type of the domain object as type arguments
+
+// Spring Data JPA will provide the implementation of this interface at runtime
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
